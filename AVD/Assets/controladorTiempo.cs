59397c11f416e9588b355f03a126b7302b5ab642
HaruKawai <angelginer27@gmail.com>
2020-02-18 17:11:16 +0100

    sprites almost

---
+++ AVD/Assets/controladorTiempo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Starting in 2 seconds.
+// a projectile will be launched every 0.3 seconds
+
+public class controladorTiempo : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        InvokeRepeating("LaunchProjectile", 1.6f, 6.0f);
+        InvokeRepeating("StopLaunchProjectile", 4.6f, 6.0f);
+        
+        InvokeRepeating("LaunchProjectile2", 0f, 2.0f);
+        InvokeRepeating("StopLaunchProjectile2", 0.5f, 2.0f);
+    }
+
+    void LaunchProjectile()
+    {
+        animator.SetBool("Walk", true);
+    }
+    void StopLaunchProjectile()
+    {
+        animator.SetBool("Walk", false);
+    }
+
+
+    void LaunchProjectile2()
+    {
+        animator.SetBool("Toma", true);
+    }
+    void StopLaunchProjectile2()
+    {
+        animator.SetBool("Toma", false);
+    }
+}
