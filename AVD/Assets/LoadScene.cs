8f6661f277d9816c3b1b11641b8fd1c5b23fef04
HaruKawai <angelginer27@gmail.com>
2020-02-20 21:04:20 +0100

    primera pract

--- AVD/Assets/LoadScene.cs
+++ AVD/Assets/LoadScene.cs
@@ -1,24 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class SceneController : MonoBehaviour
+public class LoadScene : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void quit() {
-        #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-        #else
-        Application.Quit();
-        #endif
-    }
-
     public void LoadSceneNumber(int level) 
     {
         SceneManager.LoadSceneAsync(level);
     }
-    
 }
