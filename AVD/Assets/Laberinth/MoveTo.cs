dc1e7f054d2b2ffb9b1e8d08f6d730f3b0628dfe
HaruKawai <angelginer27@gmail.com>
2020-03-20 20:16:25 +0100

    laberinth

---
+++ AVD/Assets/Laberinth/MoveTo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class MoveTo : MonoBehaviour
+{   
+    public NavMeshAgent agent;
+    RaycastHit hitInfo = new RaycastHit();
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                agent.destination = hitInfo.point;
+            }
+        }
+    }
+}
---
+++ AVD/Assets/Laberinth/SimpleGo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+public class SimpleGo : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform Destination;
+    private Transform destination1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        destination1 = Destination;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
