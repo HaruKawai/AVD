006c14c04f76faac57b99f60d38ebf7e6e8b4372
HaruKawai <angelginer27@gmail.com>
2020-04-18 13:32:08 +0200

    Almost Done

---
+++ AVD/Assets/ADV/MainScene/Torreta/Scripts/turretLauncher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretLauncher : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public Transform Hand;
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            GetComponentInParent<Animator>().SetTrigger("Turret");
+            //StartCoroutine(Shoot());
+        }
+    }
+    void LaunchTurret()
+    {
+        Instantiate(ballPrefab, Hand.position, transform.rotation);
+    }
+
+}
---
+++ AVD/Assets/ADV/MainScene/Tresure/TimelineControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class TimelineControl : MonoBehaviour
+{
+    public List<PlayableDirector> playableDirectors;
+    public PlayableDirector playableDriectorChest;
+    public TimelineAsset timelineChest;
+    public List<TimelineAsset> timelines;
+
+
+    public void Play()
+    {
+        foreach (PlayableDirector playableDirector in playableDirectors)
+        {
+            playableDirector.Play();
+        }
+    }
+
+    public void PlayChest()
+    {
+        playableDriectorChest.Play();
+    }
+
+    public void PlayFromTimelines(int index)
+    {
+        TimelineAsset selectedAsset;
+
+        if (timelines.Count <= index)
+        {
+            selectedAsset = timelines[timelines.Count - 1];
+        }
+        else
+        {
+            selectedAsset = timelines[index];
+        }
+
+        playableDirectors[0].Play(selectedAsset);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Debug.Log("disco trigger tag: "+ other.tag);
+        /*
+        if (other.CompareTag("Disco"))
+        {
+            GetComponent<Animator>().SetTrigger("dance");
+            GetComponent<TimelineControl>().Play();
+            other.GetComponentInParent<Animator>().SetTrigger("action");// in this case the animator is in the parent
+        }
+        else if (other.CompareTag("Flag"))
+        {
+            other.GetComponent<Animator>().SetBool("active", false);
+        }
+        else 
+        */if (other.CompareTag("Chest"))
+        {
+            GetComponent<Animator>().SetTrigger("takeItem");
+            other.GetComponent<Animator>().SetTrigger("Open");
+            GetComponent<TimelineControl>().PlayChest();
+        }
+    }
+
+}
